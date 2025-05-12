b7c65a2e8a98e1455d92e2e45945985ee13d9f02
Abhishek Shende <osdevisnot@gmail.com>
2025-05-12 11:14:01 -0700

    chore: update dependency versions (#52)
    * chore: update dependency versions
    
    * chore: fix build issues after dependency upgrades

--- lib/utils/response-util.ts
+++ lib/utils/response-util.ts
@@ -1,12 +1,11 @@
 import _ from 'lodash'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function extractResponseData(httpResponse: any){
+export default function extractResponseData(httpResponse: any) {
   if (_.has(httpResponse, 'data.data.items')) {
     return _.get(httpResponse, 'data.data.items')
-  } else if (_.has(httpResponse, 'data.data')){
+  } else if (_.has(httpResponse, 'data.data')) {
     return _.get(httpResponse, 'data.data')
-  }else{
+  } else {
     return httpResponse
   }
 }
